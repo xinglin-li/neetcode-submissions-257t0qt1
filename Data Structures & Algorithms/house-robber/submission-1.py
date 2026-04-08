5d42dfeb20e2e5a0dab10a48d09bbe2ddd88602c
Xinglin Li <94593915+xinglin-li@users.noreply.github.com>
2026-04-08 13:55:55 -0400

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # dp[i]: max gain from 0 to i
+        # dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0], nums[1])
+        dp = [0]*n
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        for i in range(2,n):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        return max(dp[n-1], dp[n-2])
