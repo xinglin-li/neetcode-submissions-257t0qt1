6ba65ed63b833b3bb7c04507cd90c2da6343eed9
Xinglin Li <94593915+xinglin-li@users.noreply.github.com>
2026-04-29 23:34:58 -0400

    Add: excel-sheet-column-title - submission-2

---
+++ Data Structures & Algorithms/excel-sheet-column-title/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        res = []
+        while columnNumber > 0:
+            columnNumber -= 1
+            res.append(chr(columnNumber%26 + ord("A")))
+            columnNumber //= 26
+        return "".join(reversed(res))
