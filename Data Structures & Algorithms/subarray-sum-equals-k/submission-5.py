3bf2dac56b5c6bfab8e557bd055b2fae9d20019c
Xinglin Li <94593915+xinglin-li@users.noreply.github.com>
2026-07-05 11:51:57 -0400

    Add: subarray-sum-equals-k - submission-5

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        freq = defaultdict(int)
+        freq[0] = 1
+        prefix = 0
+        ans = 0
+        for x in nums:
+            prefix += x
+            # prefix - need = k -> need = prefix - k
+            need = prefix - k
+            ans += freq[need]
+            freq[prefix] += 1
+        return ans
