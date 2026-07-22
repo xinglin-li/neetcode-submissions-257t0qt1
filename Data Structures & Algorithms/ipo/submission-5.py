acf09272640eba32cfe93708fceb428ba1e6689c
Xinglin Li <94593915+xinglin-li@users.noreply.github.com>
2026-07-22 16:54:01 -0400

    Add: ipo - submission-5

---
+++ Data Structures & Algorithms/ipo/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        projects = sorted(zip(capital, profits), key=lambda x: x[0])
+
+        max_heap = []
+        i = 0
+        n = len(projects)
+
+        for _ in range(k):
+            while i < n and projects[i][0] <= w:
+                heapq.heappush(max_heap, -projects[i][1])
+                i += 1
+            if not max_heap:
+                break
+            profit = heapq.heappop(max_heap)
+            w -= profit
+        
+        return w
