17cc535ed5c8a13c84a42b9042002aefece3aa1a
Xinglin Li <94593915+xinglin-li@users.noreply.github.com>
2026-07-27 23:04:52 -0400

    Add: valid-tree - submission-20

---
+++ Data Structures & Algorithms/valid-tree/submission-20.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # (1) math trick, valid tree has n - 1 edges. (2) valid tree no cycle, use union-find
+        if len(edges) != n - 1:
+            return False
+        
+        parent = [i for i in range(n)]
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        
+        for u, v in edges:
+            pu = find(u)
+            pv = find(v)
+            if pu == pv:
+                return False
+            parent[pu] = pv
+        
+        return True
