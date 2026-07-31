784d606338fc2fed5ad565e4375b25c15abef296
Xinglin Li <94593915+xinglin-li@users.noreply.github.com>
2026-07-31 14:18:04 -0400

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev2, prev1 = 0, 0
+        for num in nums:
+            curr = max(prev2 + num, prev1)
+            prev2 = prev1
+            prev1 = curr
+        return prev1
