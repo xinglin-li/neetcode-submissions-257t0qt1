dc84f185c00a385b2b316702b53219e17c4a36d0
Xinglin Li <94593915+xinglin-li@users.noreply.github.com>
2026-08-09 00:35:40 -0400

    Add: hand-of-straights - submission-12

---
+++ Data Structures & Algorithms/hand-of-straights/submission-12.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        # 每次都必须从当前最小的牌开始组成连续 groupSize 张。
+        # 因为当前最小牌不可能被更小的牌带走，所以它只能作为某个顺子的第一张。
+        if len(hand) % groupSize != 0:
+            return False
+        
+        count = Counter(hand)
+        min_heap = list(count.keys())
+        heapq.heapify(min_heap)
+
+        while min_heap:
+            start = min_heap[0]
+
+            for x in range(start, start + groupSize):
+                if x not in count:
+                    return False
+                
+                count[x] -= 1
+
+                if count[x] == 0:
+                    if x != min_heap[0]:
+                        return False
+                    heapq.heappop(min_heap)
+        return True
+                    
