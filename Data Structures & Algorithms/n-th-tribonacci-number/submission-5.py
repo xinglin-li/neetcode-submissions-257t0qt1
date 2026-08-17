4a7b3caa421b6aa4768d0f33ea97db8ce983a9d5
Xinglin Li <94593915+xinglin-li@users.noreply.github.com>
2026-08-16 20:50:25 -0400

    Update: n-th-tribonacci-number - submission-5

--- Data Structures & Algorithms/n-th-tribonacci-number/submission-5.py
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-5.py
@@ -1,14 +1,13 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        memo = [-1]*(n+1)
-        def dfs(i):
-            if i == 0:
-                return 0
-            elif i <= 2:
-                return 1
-            if memo[i] != -1:
-                return memo[i]
-            memo[i] = dfs(i-1) + dfs(i-2) + dfs(i-3)
-            return memo[i]
-        return dfs(n) 
+        if n == 0:
+            return 0
+        if n <= 2:
+            return 1
+        
+        # 维护前三项：T(0)=0, T(1)=1, T(2)=1
+        t0, t1, t2 = 0, 1, 1
+        for _ in range(3, n + 1):
+            t0, t1, t2 = t1, t2, t0 + t1 + t2
             
+        return t2
