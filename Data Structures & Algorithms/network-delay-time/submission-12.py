e49dce9248fc2caa95b7cb2e1616a76708c1a0ae
Xinglin Li <94593915+xinglin-li@users.noreply.github.com>
2026-09-21 16:18:58 -0400

    Add: network-delay-time - submission-12

---
+++ Data Structures & Algorithms/network-delay-time/submission-12.py
@@ -0,0 +1,25 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # Dijkstra + minHeap
+        # 1. 构建有向带权的邻接表
+        adj = defaultdict(list)
+        for u, v, w in times:
+            adj[u].append((v, w))
+        
+        # 2. Dijkstra算法: 记录各节点已经确定的最短距离
+        pq = [(0, k)] # (到达当前节点的累计耗时, 节点编号)
+        dist = {} # 充当已访问集合 + 最短耗时字典
+
+        while pq:
+            time, u = heapq.heappop(pq)
+
+            # 若该节点已被确定过最短路径, 跳过
+            if u in dist: continue
+
+            dist[u] = time
+
+            for v, w in adj[u]:
+                if v not in dist:
+                    heapq.heappush(pq, (time + w, v))
+        
+        return max(dist.values()) if len(dist) == n else -1
